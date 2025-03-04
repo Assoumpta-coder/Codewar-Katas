2ef450031834479b395aa56acb0f95819b75f1a7
Assoumpta_23 <assoumpta.uwabera22a@kepler.org>
2025-03-04 15:22:58 +0200

    Add solution - CodeHub

---
+++ 7-kyu/merge-two-arrays/merge-two-arrays.js
@@ -0,0 +1,17 @@
+function mergeArrays(a, b) {
+  let result = [];
+  let i = 0;
+​
+  while (i < a.length || i < b.length) {
+    if (i < a.length) {
+      result.push(a[i]);
+    }
+    if (i <  b.length) {
+      result.push(b[i]);
+    }
+    i++;
+  }
+​
+  return result;
+}
+​
