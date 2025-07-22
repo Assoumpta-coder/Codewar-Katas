504289cd80bb29b1ad21890f5c963f59c415254c
Assoumpta_23 <assoumpta.uwabera22a@kepler.org>
2025-07-22 16:35:03 +0200

    Update solution - CodeHub

--- 7-kyu/selective-fear-of-numbers/selective-fear-of-numbers.js
+++ 7-kyu/selective-fear-of-numbers/selective-fear-of-numbers.js
@@ -1,20 +1,18 @@
-function AmIAfraid(day, num) {
-    const lowerCaseDay = day.toLowerCase();
-    if (lowerCaseDay === 'monday') {
-        return num === 12;
-    } else if (lowerCaseDay === 'tuesday') {
-        return num > 95;
-    } else if (lowerCaseDay === 'wednesday') {
-        return num === 34;
-    } else if (lowerCaseDay === 'thursday') {
-        return num === 0;
-    } else if (lowerCaseDay === 'friday') {
-        return num % 2 === 0;
-    } else if (lowerCaseDay === 'saturday') {
-        return num === 56;
-    } else if(lowerCaseDay === 'sunday') {
-        return num === 666 || num === -666;
-    } 
-  return false
+const AmIAfraid = function(day, num){
+  if (day === 'Monday' && num === 12) {
+    return true
+  } else if (day === 'Tuesday' && num > 95) {
+    return true;
+  } else if (day === 'Wednesday' && num === 34) {
+    return true;
+  } else if (day === 'Thursday' && num === 0) {
+    return true;
+  } else if (day === 'Friday' && num % 2 === 0) {
+    return true;
+  } else if (day === 'Saturday' && num === 56) {
+    return true;
+  } else if (day === 'Sunday' && ((num === -666) || (num === 666))) {
+    return true;
   }
-​
+  return false;
+}
